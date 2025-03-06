32e0e3d567ce7e2f1a5f80b5a0e2ba8257512435
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-03-06 20:31:44 +0200

    Creating app_bar widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomedAppBar extends StatelessWidget {
+  final String text;
+  const CustomedAppBar({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+      Image.asset('assets/Left Button.png'),
+      Text(text,
+      style: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold
+      ),
+      ),
+      SizedBox(width: 10,),
+    ],);
+  }
+}
