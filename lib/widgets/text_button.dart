8573f1d4b120115fdcf07c0469e647a67ba83485
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-03-06 20:34:32 +0200

    Creating Customed TextButton

---
+++ lib/widgets/text_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextButtonGreen extends StatelessWidget {
+  final String text;
+  final VoidCallback? onTap;
+
+  const TextButtonGreen({super.key, required this.text, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 51, 134, 54),
+        ),
+      ),
+    );
+  }
+}
