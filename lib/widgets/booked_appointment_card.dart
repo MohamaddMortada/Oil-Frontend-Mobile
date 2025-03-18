d26aceaf05e4357a609e11df59a62f4813e91d77
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-03-18 21:03:37 +0200

    Creating BookedAppointmentCard

---
+++ lib/widgets/booked_appointment_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:oil_frontend_mobile/widgets/header_five.dart';
+import 'package:oil_frontend_mobile/widgets/header_four.dart';
+import 'package:oil_frontend_mobile/widgets/icon_button.dart';
+
+class BookedAppointmentCard extends StatelessWidget {
+  final String image;
+  final String clientName;
+  final String orderTime;
+  const BookedAppointmentCard({
+    super.key,
+    required this.image,
+    required this.clientName,
+    required this.orderTime,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(color: Theme.of(context).secondaryHeaderColor),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Color(0xFFFF9A17), width: 3),
+            ),
+            child: CircleAvatar(child: Image.asset(image)),
+          ),
+          SizedBox(width: 15),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HeaderFour(text: clientName),
+              HeaderFive(text: orderTime),
+            ],
+          ),
+          Spacer(),
+          Image.asset('assets/Frame 1000006492.png'),
+        ],
+      ),
+    );
+  }
+}
